539a2ba7b0ea893178a837c4b8016340615bfefd
Francisco Mota <fmota91@gmail.com>
2010-11-11 17:48:08 -0600

    docs -- Made gclist.h compliant with documentation guidelines.

--- src/fuga/gclist.h
+++ src/fuga/gclist.h
@@ -5,6 +5,7 @@
 
 /**
 *** # GC Lists
+*** ### FugaGCList
 *** 
 *** `FugaGCList` is a doubly linked list. You'll notice that there are
 *** no values other than the links forward and backward. This is because,
@@ -17,66 +18,154 @@
 *** we always refer to the dummy of the list as a "list", and to any other
 *** piece of the list as an "item", despite both being (pointers to) the
 *** same structure.
+***
+*** - Fields:
+***     - `FugaGCList* next`: the next item in the list, or the first if this
+***     is the dummy.
+***     - `FugaGCList* prev`: the previous item in the list, or the last if
+***     this is the dummy.
 **/
 typedef struct FugaGCList FugaGCList;
 struct FugaGCList {
-    FugaGCList *next;
-    FugaGCList *prev;
+    FugaGCList* next;
+    FugaGCList* prev;
 };
 
 /**
-*** `FugaGCList_init` initializes (or reinitializes) a list. This involves
-*** making the list a list of one. I.e., the dummy points only to itself.
+*** ## Constructor
+*** ### FugaGCList_init
+***
+*** Initializes (or reinitialize) a list. This involves making the list a
+*** list of one (itself). Or you could view it as an empty list. I.e. the
+*** dummy points only to itself.
+***
+*** - Parameters:
+***     - `FugaGCList* list`: The list to initialize. It is modified.
+*** - Return value: void
+*** - See also: `FugaGCList`
 **/
 void FugaGCList_init (FugaGCList* list);
 
 /**
-*** `FugaGCList_pushBack` places an item at the back of the list. Likewise,
-*** `FugaGCList_pushFront` places an item at the front of the list.
+*** ## Adding Items
+*** ### FugaGCList_pushBack
+***
+*** Place an item at the end of the list.
+***
+*** - Parameters:
+***     - `FugaGCList* list`: pointer to the dummy of the list.
+***     - `FugaGCList* item`: the item to be placed at the end.
+*** - See also: `FugaGCList_pushFront`
 **/
-void FugaGCList_pushBack  (FugaGCList* list, FugaGCList* item);
-void FugaGCList_pushFront (FugaGCList* list, FugaGCList* item);
+void FugaGCList_pushBack(FugaGCList* list, FugaGCList* item);
 
 /**
-*** `FugaGCList_unlink` removes an item from its surrounding list. The item's
-*** links are unchanged, so know that `item->prev` and `item->next` are
-*** consequently wrong.
+*** ### FugaGCList_pushFront
+***
+*** Place an item at the start of the list.
+***
+*** - Parameters:
+***     - `FugaGCList* list`: pointer to the dummy of the list.
+***     - `FugaGCList* item`: the item to be placed at the start.
+*** - See also: `FugaGCList_pushBack`
+**/
+void FugaGCList_pushFront(FugaGCList* list, FugaGCList* item);
+
+/**
+*** ## Removing Items
+*** ### FugaGCList_unlink
+***
+*** Remove an item from its surrounding list. The item's links are unchanged,
+*** so know that its `next` and `prev` fields are going to be wrong.
+***
+*** - Parameters:
+***     - `FugaGCList* item`: The item to unlink.
+*** - Returns: void
+*** - See also: `FugaGCList_popFront`, `FugaGCList_popBack`
 **/
 void FugaGCList_unlink (FugaGCList* item);
 
 /**
-*** `FugaGCList_popFront` removes an item from the front of the list, and
-*** returns it. `FugaGCList_popBack` does the same, but to the back of the
-*** list instead.
+*** ### FugaGCList_popFront
 ***
-*** The returned item's links are not modified, so `item->prev` and
-*** `item->next` are both wrong.
+*** Remove the first item from the list, returning it. Note that the returned
+*** item's `next` and `prev` remain unchanged, so they are both wrong 
+*** (usually).
+***
+*** - Parameters:
+***     - `FugaGCList* list`: The list from which to remove the first item.
+*** - Returns: a pointer to the removed item with `next` and `prev` unchanged.
+*** - See Also: `FugaGCList_popBack`, `FugaGCList_unlink`
 **/
-FugaGCList* FugaGCList_popFront (FugaGCList* list);
-FugaGCList* FugaGCList_popBack  (FugaGCList* list);
+FugaGCList* FugaGCList_popFront(FugaGCList* list);
 
 /**
-*** `FugaGCList_appendFront` takes an existing list and appends it to the
-*** front of another list. `FugaGCList_appendBack` does the same, but to
-*** the back.
+*** ### FugaGCList_popBack
 ***
-*** The `dest` list is the one that grows. The `src` list is reinitialized
-*** in order to maintain sanity.
+*** Remove the last item from the list, returning it. Note that the returned
+*** item's `next` and `prev` remain unchanged, so they are both wrong 
+*** (usually).
+***
+*** - Parameters:
+***     - `FugaGCList* list`: The list from which to remove the lastt item.
+*** - Returns: a pointer to the removed item with `next` and `prev` unchanged.
+*** - See Also: `FugaGCList_popFront`, `FugaGCList_unlink`
 **/
-void FugaGCList_appendFront (FugaGCList* dest, FugaGCList* src);
-void FugaGCList_appendBack  (FugaGCList* dest, FugaGCList* src);
+FugaGCList* FugaGCList_popBack(FugaGCList* list);
 
+/**
+*** ## Appending
+*** ### FugaGCList_appendFront
+***
+*** Concatenate the `src` list with the `dest` list (in that order), putting
+*** the result in `dest`, and leaving the `src` list empty.
+***
+*** - Parameters:
+***     - `FugaGCList* dest`: The destination list, which ends up with `src`
+***     concatenated at the beginning.
+***     - `FugaGCList* src`: The source list, which ends up empty.
+*** - Returns: void
+*** - See Also: `FugaGCList_appendBack`
+**/
+void FugaGCList_appendFront(FugaGCList* dest, FugaGCList* src);
+
+/**
+*** ### FugaGCList_appendBack
+***
+*** Concatenate the `dest` list with the `src` list (in that order), putting
+*** the result in `dest`, and leaving the `src` list empty.
+***
+*** - Parameters:
+***     - `FugaGCList* dest`: The destination list, which ends up with `src`
+***     concatenated at the end.
+***     - `FugaGCList* src`: The source list, which ends up empty.
+*** - Returns: void
+*** - See Also: `FugaGCList_appendFront`
+**/
+void FugaGCList_appendBac(FugaGCList* dest, FugaGCList* src);
 
 /**
 *** ## Properties
+*** ### FugaGCList_empty
 ***
-*** `FugaGCList_empty` determines whether a list is empty.
-*** returns TRUE (non-0) if it is, and FALSE(0) otherwise
+*** Determine whether the list is empty.
 ***
-*** `FugaGCList_contains` determines whether data is a member of the list.
-*** returns TRUE (non-0) if it is, and FALSE (0) otherwise.
+*** - Parameters:
+***     - `FugaGCList* list`: the list.
+*** - Returns: true if the list is empty, false otherwise.
 **/
-bool FugaGCList_empty   (FugaGCList* list);
+bool FugaGCList_empty(FugaGCList* list);
+
+/**
+*** ### FugaGCList_contains
+*** 
+*** Determine whether an item is a member of the list.
+*** 
+*** - Parameters:
+***     - `FugaGCList* list`: the list to search in.
+***     - `void* data`: the item to search for.
+*** - Returns: true if the item is in the list, false otherwise.
+**/
 bool FugaGCList_contains(FugaGCList* list, void* data);
 
 #endif
