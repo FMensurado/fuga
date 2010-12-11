fa93414c86ea3b7f88dd5aea7d835a755771bf66
Francisco Mota <fmota91@gmail.com>
2010-12-11 08:50:30 -0600

    Fully featured PEG.

--- peg.py
+++ peg.py
@@ -37,12 +37,14 @@ class PEG(object):
         if (name,index) in self._memo:
             return self._memo[(name,index)]
         match = self._matcher[name](code, index, self)
+        if match.success and hasattr(self, name):
+            match.value = getattr(self, name)(match.value)
         self._memo[(name,index)] = match
         return match
 
-    def parse(self, code):
+    def parse(self, code, name='start'):
         self._reset()
-        match = self._match('start', code, 0)
+        match = self._match(name, code, 0)
         self._reset()
         if match.success:
             return match.value
@@ -95,12 +97,15 @@ class Match(object):
         return "Match(%r, %r, %r)" % (self.success, self.index, self.value)
 
     def errormsg(self):
-        return "expected %s" ', '.join([repr(c) for c in self.value])
+        return "expected %s" % ', '.join([repr(c) for c in self.value])
 
 
 class Matcher(object):
     def __call__(self, code, index, grammar):
         return self.match(code, index, grammar)
+
+    def reprnp(self):
+        return repr(self)
 
     def star(self):
         return _star(self)
@@ -120,7 +125,10 @@ class sym(Matcher):
     def __init__(self, name):
         self.name = name
 
-    def __call__(self, code, index, grammar):
+    def __repr__(self):
+        return 'sym(%r)' % self.name
+
+    def match(self, code, index, grammar):
         return grammar._match(self.name, code, index)
 
 class lit(Matcher):
@@ -140,6 +148,9 @@ class lit(Matcher):
     def __init__(self, string):
         self.string = string
 
+    def __repr__(self):
+        return "lit(%r)" % self.string
+
     def match(self, code, index, grammar):
         endindex = index + len(self.string)
         if self.string == code[index:endindex]:
@@ -157,6 +168,9 @@ class dot(Matcher):
     >>> dot().match('10', 2, None)
     Match(False, 2, [])
     """
+    def __repr__(self):
+        return "dot()"
+
     def match(self, code, index, grammar):
         if index < len(code):
             return Match(True, index+1, code[index])
@@ -207,6 +221,9 @@ class cc(Matcher):
                 self.charset.add(desc[i])
             i += 1
 
+    def __repr__(self):
+        return "cc(%r)" % self.desc
+
     def match(self, code, index, grammar):
         if index >= len(code):
             return Match(False, index, ['[%s]' % self.desc])
@@ -233,6 +250,9 @@ class _star(Matcher):
     """
     def __init__(self, matcher):
         self.matcher = matcher
+
+    def __repr__(self):
+        return '%r.star()' % self.matcher
 
     def match(self, code, index, grammar):
         results = []
@@ -262,6 +282,9 @@ class _plus(Matcher):
     def __init__(self, matcher):
         self.matcher = matcher
 
+    def __repr__(self):
+        return '%r.plus()' % self.matcher
+
     def match(self, code, index, grammar):
         results = []
         match = self.matcher(code, index, grammar)
@@ -284,6 +307,9 @@ class _opt(Matcher):
     def __init__(self, matcher):
         self.matcher = matcher
 
+    def __repr__(self):
+        return repr(self.matcher) + '.opt()'
+
     def match(self, code, index, grammar):
         match = self.matcher(code, index, grammar)
         if match.success:
@@ -303,6 +329,9 @@ class seq(Matcher):
     """
     def __init__(self, *matchers):
         self.matchers = matchers
+
+    def __repr__(self):
+        return 'seq(%s)'%', '.join([v.reprnp() for v in self.matchers])
 
     def match(self, code, index, grammar):
         results = []
@@ -335,6 +364,12 @@ class _or(Matcher):
         self.left = left
         self.right = right
 
+    def reprnp(self):
+        return '%s / %s' % (self.left.reprnp(), self.right.reprnp())
+
+    def __repr__(self):
+        return '(%s)' % self.reprnp()
+
     def match(self, code, index, grammar):
         leftm = self.left.match(code, index, grammar)
         if leftm.success:
@@ -359,6 +394,9 @@ class andp(Matcher):
     def __init__(self, matcher):
         self.matcher = matcher
 
+    def __repr__(self):
+        return 'andp(%s)' % self.matcher.reprnp()
+
     def match(self, code, index, grammar):
         match = self.matcher(code, index, grammar)
         if match.success:
@@ -381,6 +419,9 @@ class notp(Matcher):
     def __init__(self, matcher):
         self.matcher = matcher
 
+    def __repr__(self):
+        return 'notp(%s)' % self.matcher.reprnp()
+
     def match(self, code, index, grammar):
         match = self.matcher(code, index, grammar)
         if match.success:
@@ -389,11 +430,190 @@ class notp(Matcher):
             return Match(True, index, None)
 
 
+class PE_parser(PEG):
+    m_start = seq(sym('Expression'), sym('EndOfFile'))
+
+    def start(self, v):
+        return v[0]
+
+    # Hierarchical
+    m_Expression = seq(sym('Sequence'),
+                       seq(sym('SLASH'),
+                           sym('Sequence')).star())
+    m_Sequence   = sym('Prefix').star()
+    m_Prefix     = seq((sym('AND') / sym('NOT')).opt(),
+                       sym('Suffix'))
+    m_Suffix     = seq(sym('Primary'),
+                       (sym('QUESTION') / sym('STAR') / sym('PLUS')).opt())
+    m_Primary    = (sym('Identifier') /
+                    seq(sym('OPEN'), sym('Expression'), sym('CLOSE')) /
+                    sym('Literal') / sym('Class') / sym('DOT'))
+
+    def Expression(self, v):
+        result = v[0]
+        for w in v[1]:
+            result = result / w[1]
+        return result
+
+    def Sequence(self, v):
+        if len(v) == 1:
+            return v[0]
+        else:
+            return seq(*v)
+    
+    def Prefix(self, v):
+        if v[0] is None:
+            return v[1]
+        prefix = {'&': andp, '!': notp}
+        return prefix[v[0][0]](v[1])
+
+    def Suffix(self, v):
+        if v[1] is None:
+            return v[0]
+        suffix = {'?': 'opt', '*': 'star', '+': 'plus'}
+        return getattr(v[0], suffix[v[1][0]])()
+
+    def Primary(self, v):
+        if isinstance(v, tuple):
+            return v[1]
+        else:
+            return v
+   
+    # Lexical
+    m_Identifier = seq(cc('a-zA-Z_'),
+                       cc('a-zA-Z_0-9').star(),
+                       sym('Spacing'))
+
+    m_Literal    = (seq(lit("'"), seq(notp(lit("'")), sym("Char")).star(),
+                        lit("'"), sym("Spacing")) /
+                    seq(lit('"'), seq(notp(lit('"')), sym("Char")).star(),
+                        lit('"'), sym("Spacing")))
+    m_Class      = seq(lit('['), seq(notp(lit(']')), sym("Range")).star(),
+                       lit(']'), sym("Spacing"))
+
+    m_Range      = (seq(sym('Char'), lit('-'), sym('Char')) /
+                    seq(sym('Char')))
+
+    m_Char       = (seq(lit("\\"), cc('\n\t\r\a\'\"\[\]\\-')) /
+                    seq(notp(lit("\\")), dot()))
+
+
+    def Identifier(self, v):
+        return sym(v[0] + ''.join(v[1]))
+
+    def Literal(self, v):
+        return lit(''.join([w[1] for w in v[1]]))
+
+    def Class(self, v):
+        return cc(''.join([w[1] for w in v[1]]))
+
+    def Range(self, v):
+        return ''.join(v)
+
+    def Char(self, v):
+        escapes = { 'n': '\n', 'r': '\r', 't': '\t',
+                    "'": "'",  '"': '"', '[': '[', ']': ']',
+                    '\\': '\\', '-': '\-' }
+        return escapes.get(v[1], v[1])
+    
+    m_SLASH      = seq(lit('/'), sym('Spacing'))
+    m_AND        = seq(lit('&'), sym('Spacing'))
+    m_NOT        = seq(lit('!'), sym('Spacing'))
+    m_QUESTION   = seq(lit('?'), sym('Spacing'))
+    m_STAR       = seq(lit('*'), sym('Spacing'))
+    m_PLUS       = seq(lit('+'), sym('Spacing'))
+    m_OPEN       = seq(lit('('), sym('Spacing'))
+    m_CLOSE      = seq(lit(')'), sym('Spacing'))
+    m_DOT        = seq(lit('.'), sym('Spacing'))
+
+    def DOT(self, v):
+        return dot()
+
+    m_Spacing    = cc(' \t\r\n').star()
+    m_EndOfFile  = notp(dot())
+
 def PE(matcher):
+    r"""
+    >>> PE('a')
+    sym('a')
+    >>> PE('a b c')
+    seq(sym('a'), sym('b'), sym('c'))
+    >>> PE('')
+    seq()
+    >>> PE('a?')
+    sym('a').opt()
+    >>> PE('a*')
+    sym('a').star()
+    >>> PE('a+')
+    sym('a').plus()
+    >>> PE('&a')
+    andp(sym('a'))
+    >>> PE('!a')
+    notp(sym('a'))
+    >>> PE('a / b')
+    (sym('a') / sym('b'))
+    >>> PE('a / b / c')
+    (sym('a') / sym('b') / sym('c'))
+    >>> PE("'abc'")
+    lit('abc')
+    >>> PE('"abc"')
+    lit('abc')
+    >>> PE('"abc\\""')
+    lit('abc"')
+    >>> PE('"\-"')
+    lit('\\-')
+    >>> PE('.')
+    dot()
+    >>> PE('[abc]')
+    cc('abc')
+    >>> PE('[a-zA-Z]')
+    cc('a-zA-Z')
+    >>> PE('a (b c)*')
+    seq(sym('a'), seq(sym('b'), sym('c')).star())
+    """
     if isinstance(matcher, Matcher):
         return matcher
+    elif isinstance(matcher, str):
+        return PE_parser().parse(matcher)
     raise TypeError, "expected a Matcher"
 
+
+class Calc(PEG):
+    """
+    >>> calc = Calc()
+    >>> calc.parse('10')
+    10
+    >>> calc.parse('1+1')
+    2
+    >>> calc.parse('5*5')
+    25
+    >>> calc.parse('1+2*3+4*5')
+    27
+    >>> calc.parse('(1+2)*(3+4)*5')
+    105
+    """
+
+    m_start = 'spacing plus eof'
+    m_plus  = 'times (PLUS times)*'
+    m_times = 'term (TIMES term)*'
+    m_term  = 'pexp / num'
+    m_pexp  = 'OPEN plus CLOSE'
+    m_num   = '[0-9]+ spacing'
+
+    m_PLUS  = '"+" spacing'
+    m_TIMES = '"*" spacing'
+    m_OPEN  = '"(" spacing'
+    m_CLOSE = '")" spacing'
+
+    m_spacing = '[ \t\n\r]*'
+    m_eof = r'!.'
+
+    def start(self,v): return v[1]
+    def plus(self,v):  return reduce(lambda x,y: x + y[1], v[1], v[0])
+    def times(self,v): return reduce(lambda x,y: x * y[1], v[1], v[0])
+    def pexp(self,v):  return v[1]
+    def num(self,v):   return int(''.join(v[0]))
+
 if __name__ == '__main__':
-   testmod()
+    testmod()
 
