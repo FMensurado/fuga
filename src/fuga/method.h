02ea19b048d80196302a0dfcd5758c2392580d79
Francisco Mota <fmota91@gmail.com>
2011-03-18 12:26:06 -0500

    Added casts to FUGA_METHOD macros.
    This makes them much nicer to use, and allows us to use type-specific
    functions.

--- src/fuga/method.h
+++ src/fuga/method.h
@@ -5,20 +5,26 @@
 
 void FugaMethod_init(void* self);
 
-void* FugaMethod_new_(void* self, void* (*)(void*, void*, void*));
-void* FugaMethod_call(void* self, void* recv, void* args);
-void* FugaMethodN_new_(void* self, void* (*)(void*, void*));
-void* FugaMethod0_new_(void* self, void* (*)(void*));
-void* FugaMethod1_new_(void* self, void* (*)(void*, void*));
-void* FugaMethod2_new_(void* self, void* (*)(void*, void*, void*));
-void* FugaMethodStr_new_(void* self, void* (*)(void*));
-void* FugaMethod_method(void* scope, void* args, void* body);
+typedef void* (*FugaMethodFn)    (void*, void*, void*);
+typedef void* (*FugaMethodFnN)   (void*, void*);
+typedef void* (*FugaMethodFn0)   (void*);
+typedef void* (*FugaMethodFn1)   (void*, void*);
+typedef void* (*FugaMethodFn2)   (void*, void*, void*);
 
-#define FUGA_METHOD(fn) (FugaMethodN_new_(self, (fn)))
-#define FUGA_METHOD_0ARG(fn) (FugaMethod0_new_(self, (fn)))
-#define FUGA_METHOD_1ARG(fn) (FugaMethod1_new_(self, (fn)))
-#define FUGA_METHOD_2ARG(fn) (FugaMethod2_new_(self, (fn)))
-#define FUGA_METHOD_STR(fn) (FugaMethodStr_new_(self, (fn)))
+void* FugaMethod_new_(void* self, FugaMethodFn);
+void* FugaMethodN_new_(void* self, FugaMethodFnN);
+void* FugaMethod0_new_(void* self, FugaMethodFn0);
+void* FugaMethod1_new_(void* self, FugaMethodFn1);
+void* FugaMethod2_new_(void* self, FugaMethodFn2);
+void* FugaMethodStr_new_(void* self, FugaMethodFn0);
+void* FugaMethod_method(void* scope, void* args, void* body);
+void* FugaMethod_call(void* self, void* recv, void* args);
+
+#define FUGA_METHOD(fn) (FugaMethodN_new_(self, (FugaMethodFnN)(fn)))
+#define FUGA_METHOD_0ARG(fn) (FugaMethod0_new_(self,(FugaMethodFn0)(fn)))
+#define FUGA_METHOD_1ARG(fn) (FugaMethod1_new_(self,(FugaMethodFn1)(fn)))
+#define FUGA_METHOD_2ARG(fn) (FugaMethod2_new_(self,(FugaMethodFn2)(fn)))
+#define FUGA_METHOD_STR(fn) (FugaMethodStr_new_(self,(FugaMethodFn0)fn))
 
 
 #endif
