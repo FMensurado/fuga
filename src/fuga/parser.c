4b0bf79c04c2bf29e094c02413bc828639af7883
Francisco Mota <fmota91@gmail.com>
2011-04-20 06:47:47 -0500

    Rewrote parser for simpler grammar.

--- src/fuga/parser.c
+++ src/fuga/parser.c
@@ -3,7 +3,6 @@
 #include "test.h"
 
 struct FugaParser {
-    void* operators;
     FugaLexer* lexer;
 };
 
@@ -11,7 +10,6 @@ void FugaParser_mark(
     void *_parser
 ) {
     FugaParser* parser = _parser;
-    Fuga_mark_(parser, parser->operators);
     Fuga_mark_(parser, parser->lexer);
 }
 
@@ -20,26 +18,6 @@ FugaParser* FugaParser_new(
 ) {
     FugaParser* parser = Fuga_clone_(FUGA->Object, sizeof(FugaParser));
     Fuga_onMark_(parser, FugaParser_mark);
-    parser->operators = Fuga_clone(FUGA->Object);
-
-    // relational: 500s
-    FugaParser_infix_precedence_(parser, "==", 500);
-    FugaParser_infix_precedence_(parser, "<=", 500);
-    FugaParser_infix_precedence_(parser, ">=", 500);
-    FugaParser_infix_precedence_(parser, "<",  500);
-    FugaParser_infix_precedence_(parser, ">",  500);
-    // collections: 600s
-    FugaParser_infix_precedence_(parser, "++", 600);
-    // default: 1000s
-    // arithmetic: 1500s
-    FugaParser_infix_precedence_(parser, "+",  1500);
-    FugaParser_infix_precedence_(parser, "-",  1500);
-    FugaParser_infix_precedence_(parser, "*",  1510);
-    FugaParser_infix_precedence_(parser, "/",  1510);
-    FugaParser_infix_precedence_(parser, "//", 1510);
-    FugaParser_infix_precedence_(parser, "%",  1510);
-    FugaParser_infix_precedence_(parser, "**", 1520);
-
     return parser;
 }
 
@@ -66,78 +44,21 @@ void FugaParser_parseTokens_(
     parser->lexer = lexer;
 }
 
-void* FugaParser_operators(
-    FugaParser* self
-) {
-    return self->operators;
-}
-
-void FugaParser_infix_precedence_(
+bool FugaParser_check_(
     FugaParser* parser,
-    const char* op,
-    size_t precedence
+    FugaTokenType tokenType
 ) {
-    void* self = parser->operators;
-    void* opSymbol = FUGA_SYMBOL(op);
-    void* opData;
-    void* precedenceSymbol = FUGA_SYMBOL("precedence");
-    if (Fuga_has(self, opSymbol)) {
-        opData = Fuga_get(self, opSymbol);
-        opData = Fuga_need(opData);
-        if (!Fuga_isRaised(opData)) {
-            Fuga_set(opData, precedenceSymbol, FUGA_INT(precedence));
-            return;
-        }
-    } 
-    opData = Fuga_clone(FUGA->Object);
-    Fuga_set(opData, precedenceSymbol, FUGA_INT(precedence));
-    Fuga_set(self, opSymbol, opData);
-}
-
-size_t FugaParser_precedence_(
-    FugaParser* parser,
-    const char* op
-) {
-    void* self = parser->operators;
-    void* opdata = Fuga_get(self, FUGA_SYMBOL(op));
-    if (!Fuga_isRaised(opdata)) {
-        void* precedence = Fuga_get(opdata, FUGA_SYMBOL("precedence"));
-        precedence = Fuga_need(precedence);
-        if (Fuga_isInt(precedence)) {
-            long p = FugaInt_value(precedence);
-            if (p > 1999) p = 1999;
-            if (p < 2)    p = 2;
-            return p;
-        }
+    ALWAYS(parser);
+    if (tokenType == FugaLexer_peek(parser->lexer)->type) {
+        return true;
     }
-    return 1000; // default precedence
+    return false;
 }
 
-/**
-*** ### _FugaParser_lbp_
-*** 
-*** Get the left binding power of a given token (within a parser
-*** context). This tells us which kinds of expressions the token can be
-*** a part of.
-**/
-size_t _FugaParser_lbp_(
-    FugaParser* parser,
-    FugaToken* token
+void FugaParser_advance(
+    FugaParser* parser
 ) {
-    switch (token->type) {
-    case FUGA_TOKEN_ERROR:    return 9999;
-    case FUGA_TOKEN_LPAREN:   return 2000;
-    case FUGA_TOKEN_LBRACKET: return 2000;
-    case FUGA_TOKEN_LCURLY:   return 2000;
-    case FUGA_TOKEN_INT:      return 2000;
-    case FUGA_TOKEN_STRING:   return 2000;
-    case FUGA_TOKEN_SYMBOL:   return 2000;
-    case FUGA_TOKEN_NAME:     return 2000;
-    case FUGA_TOKEN_OP:       return 1000;
-/*  case FUGA_TOKEN_OP:         
-        return FugaParser_precedence_(parser, token->value); */
-    default: return 0;
-    }
+    FugaLexer_next(parser->lexer);
 }
 
 bool FugaParser_advance_(
@@ -153,12 +74,9 @@ bool FugaParser_advance_(
 }
 
 void* FugaParser_error_(
-    FugaParser* parser,
+    FugaParser* self,
     const char* message
 ) {
-    ALWAYS(parser); ALWAYS(message);
-    void* self = parser->operators;
-    ALWAYS(self);
     FUGA_RAISE(FUGA->SyntaxError, message);
 }
 
@@ -187,158 +105,154 @@ void* FugaParser_unfinished_(
             return FugaParser_unfinished_(parser, "expected " name); \
     } while(0)
 
-void* _FugaParser_buildExpr_(
-    void* self,
-    void* msg
+
+void* FugaParser_object(
+    FugaParser* self
 ) {
-    FUGA_CHECK(self);
-    FUGA_CHECK(msg);
-    if (Fuga_isExpr(self)) {
-        FUGA_CHECK(Fuga_append_(self, msg));
-        return self;
-    } else { 
-        void* expr = Fuga_clone(FUGA->Expr);
-        FUGA_CHECK(Fuga_append_(expr, self));
-        FUGA_CHECK(Fuga_append_(expr, msg));
-        return expr;
-    }
+    void* block;
+    FUGA_PARSER_EXPECT(self, FUGA_TOKEN_LPAREN, "(");
+    FUGA_CHECK(block = FugaParser_block(self));
+    FUGA_PARSER_EXPECT_UNFINISHED(self, FUGA_TOKEN_RPAREN, ")");
+    return block;
 }
 
-/**
-*** ### _FugaParser_derive_
-*** 
-*** Derive an expression or a value that begins with the given token.
-**/
-void* _FugaParser_derive_(
-    FugaParser* parser,
-    FugaToken* token
+void* FugaParser_msg(
+    FugaParser* self
 ) {
-    // FIXME: use more descriptive error messages.
-    void* self = parser->operators;
+    FugaToken* token = FugaLexer_next(self->lexer);
+    if (token->type != FUGA_TOKEN_NAME)
+        FUGA_RAISE(FUGA->SyntaxError, "expected msg");
+    FugaMsg* msg = FugaMsg_fromSymbol(FugaToken_symbol(token));
+    FUGA_CHECK(msg);
+
+    if (FugaParser_check_(self, FUGA_TOKEN_LPAREN)) {
+        void* block = FugaParser_object(self);
+        FUGA_CHECK(block);
+        FUGA_HEADER(msg)->slots = FUGA_HEADER(block)->slots;
+    }
+
+    return msg;
+}
+
+void* FugaParser_root(
+    FugaParser* self
+) {
+    FugaToken* token = FugaLexer_peek(self->lexer);
+    void* value;
+
     switch (token->type) {
-    case FUGA_TOKEN_ERROR:
-        FUGA_RAISE(FUGA->SyntaxError, "invalid token");
-    
     case FUGA_TOKEN_LPAREN:
-        self = FugaParser_block(parser);
-        FUGA_CHECK(self);
-        FUGA_PARSER_EXPECT_UNFINISHED(parser, FUGA_TOKEN_RPAREN, ")");
-        return self;
+        return FugaParser_object(self);
+
+    case FUGA_TOKEN_NAME:
+        return FugaParser_msg(self);
 
     case FUGA_TOKEN_LBRACKET:
-        self = FugaParser_expression(parser);
-        FUGA_CHECK(self);
-        FUGA_PARSER_EXPECT_UNFINISHED(parser, FUGA_TOKEN_RBRACKET, "]");
-        return self;
+        FugaParser_advance(self);
+        FUGA_CHECK(value = FugaParser_expression(self));
+        FUGA_PARSER_EXPECT(self, FUGA_TOKEN_RBRACKET, "]");
+        return value;
 
-    case FUGA_TOKEN_INT:    return FugaToken_int(token);
-    case FUGA_TOKEN_STRING: return FugaToken_string(token);
-    case FUGA_TOKEN_SYMBOL: return FugaToken_symbol(token);
+    case FUGA_TOKEN_INT:
+        FugaParser_advance(self);
+        return FugaToken_int(token);
 
-    case FUGA_TOKEN_NAME:
-        self = FugaMsg_fromSymbol(FugaToken_symbol(token));
-        FUGA_CHECK(self);
-        if (FugaParser_advance_(parser, FUGA_TOKEN_LPAREN)) {
-            void* slots = FugaParser_block(parser);
-            FUGA_CHECK(slots);
-            FUGA_PARSER_EXPECT_UNFINISHED(parser, FUGA_TOKEN_RPAREN, ")");
-            FUGA_HEADER(self)->slots = FUGA_HEADER(slots)->slots;
+    case FUGA_TOKEN_STRING:
+        FugaParser_advance(self);
+        return FugaToken_string(token);
+
+    case FUGA_TOKEN_SYMBOL:
+        FugaParser_advance(self);
+        return FugaToken_symbol(token);
+
+    case FUGA_TOKEN_OP:
+        FugaParser_advance(self);
+        void* msg = FugaMsg_fromSymbol(FugaToken_symbol(token));
+        FUGA_CHECK(msg);
+        FUGA_CHECK(Fuga_append_(msg, FugaParser_root(self)));
+        return msg;
+
+    default:
+        FUGA_RAISE(FUGA->SyntaxError,
+            "expected [, (, NAME, INT, STRING, or SYMBOL"
+        );
+    }
+
+}
+
+void* FugaParser_part(
+    FugaParser* self
+) {
+    void* root = FugaParser_root(self);
+    FUGA_CHECK(root);
+    void* expr = NULL;
+
+    while (FugaParser_check_(self, FUGA_TOKEN_NAME)) {
+        if (!expr) {
+            expr = Fuga_clone(FUGA->Expr);
+            FUGA_CHECK(Fuga_append_(expr, root));
         }
-        return self;
-
-    case FUGA_TOKEN_OP:
-        self = FugaMsg_fromSymbol(FugaToken_symbol(token));
-        FUGA_CHECK(self);
-        void* part = FugaParser_expression_(parser, 2000);
-        FUGA_CHECK(Fuga_append_(self, part));
-        return self;
-
-    default:
-        FUGA_RAISE(FUGA->SyntaxError, "invalid syntax");
+        void* msg = FugaParser_msg(self);
+        FUGA_CHECK(msg);
+        FUGA_CHECK(Fuga_append_(expr, msg));
     }
-
-}
-
-/**
-*** ### _FugaParser_derive_after_
-*** 
-*** Derive the continuation of an expression or value.
-**/
-void* _FugaParser_derive_after_(
-    FugaParser* parser,
-    FugaToken* token,
-    void* expr
-) {
-    void* self = parser->operators;
-    switch (token->type) {
-    case FUGA_TOKEN_ERROR:
-        FUGA_RAISE(FUGA->SyntaxError, "invalid token");
-    
-    case FUGA_TOKEN_NAME:
-        self = _FugaParser_derive_(parser, token);
-        return _FugaParser_buildExpr_(expr, self);
-
-    case FUGA_TOKEN_OP:
-        self = FugaMsg_fromSymbol(FugaToken_symbol(token));
-        FUGA_CHECK(Fuga_append_(self, expr));
-        FUGA_CHECK(Fuga_append_(self,
-            FugaParser_expression_(parser, 1000)
-        ));
-
-
-        return self;
-
-    case FUGA_TOKEN_LCURLY:
-        self = FugaParser_block(parser);
-        FUGA_CHECK(self);
-        FUGA_PARSER_EXPECT_UNFINISHED(parser, FUGA_TOKEN_RCURLY, "}");
-        void* result = FUGA_MSG("def");
-        Fuga_append_(result, expr);
-        Fuga_extend_(result, self);
-        return result;
-
-    default:
-        FUGA_RAISE(FUGA->SyntaxError, "invalid syntax");
-    }
-}
-
-/**
-*** ### FugaParser_expression_
-*** 
-*** Get the next expression in the stream.
-*** 
-*** - Params:
-***     - FugaParser* parser
-***     - size_t rbp: the right binding power. Determines which kinds of
-***     tokens we consider to be part of our expression.
-*** - Return: the expression as a Fuga object.
-**/
-void* FugaParser_expression_(
-    FugaParser* parser,
-    size_t rbp
-) {
-    ALWAYS(parser);
-    ALWAYS(parser->lexer);
-
-    FugaToken* token = FugaLexer_next(parser->lexer);
-    void* self = _FugaParser_derive_(parser, token);
-    FUGA_CHECK(self);
-    token = FugaLexer_peek(parser->lexer);
-    while (rbp < _FugaParser_lbp_(parser, token)) {
-        token = FugaLexer_next(parser->lexer);
-        self = _FugaParser_derive_after_(parser, token, self);
-        FUGA_CHECK(self);
-        token = FugaLexer_peek(parser->lexer);
-    }
-    if (token->type == FUGA_TOKEN_ERROR)
-        FUGA_RAISE(FUGA->SyntaxError, "invalid token");
-    return self;
+    return expr ? expr : root;
 }
 
 void* FugaParser_expression(
-    FugaParser* parser
+    FugaParser* self
 ) {
-    return FugaParser_expression_(parser, 0);
+    void* part = FugaParser_part(self);
+    FUGA_CHECK(part);
+
+    if (FugaParser_advance_(self, FUGA_TOKEN_LCURLY)) {
+        void* body = FugaParser_block(self);
+        FUGA_CHECK(body);
+        FUGA_PARSER_EXPECT_UNFINISHED(self, FUGA_TOKEN_RCURLY, "}");
+        void* method = FUGA_MSG("def");
+        FUGA_CHECK(Fuga_append_(method, part));
+        FUGA_CHECK(Fuga_extend_(method, body));
+        return method;
+    }
+
+    while (FugaParser_check_(self, FUGA_TOKEN_OP)) {
+        FugaToken *token = FugaLexer_next(self->lexer);
+        void* msg = FugaMsg_fromSymbol(FugaToken_symbol(token));
+        FUGA_CHECK(Fuga_append_(msg, part));
+        FUGA_CHECK(Fuga_append_(msg, FugaParser_part(self)));
+        part = msg;
+    }
+
+    return part;
+}
+
+void* FugaParser_slot_(
+    FugaParser* self,
+    void* block
+) {
+    void* doc = NULL;
+    while (FugaParser_check_(self, FUGA_TOKEN_DOC)) {
+        FugaToken *token = FugaLexer_next(self->lexer);
+        FugaString *str = FugaToken_string(token);
+        if (doc)
+            doc = FugaString_cat_(doc, str);
+        else
+            doc = str;
+        FugaParser_advance_(self, FUGA_TOKEN_SEPARATOR);
+    }
+
+    if (FugaParser_check_(self, FUGA_TOKEN_END)) {
+        return FugaParser_unfinished_(
+            self,
+            "expected expression after slot documentation"
+        );
+    }
+
+    FUGA_CHECK(Fuga_append_(block, FugaParser_expression(self)));
+    if (doc) {
+        FUGA_CHECK(Fuga_setDocI(block, -1, doc));
+    }
+    return FUGA->nil;
 }
 
 void* FugaParser_block(
@@ -364,9 +278,7 @@ void* FugaParser_block(
                 "expected separator between slots"
             );
 
-        void* slot = FugaParser_expression(parser);
-        FUGA_CHECK(slot);
-        Fuga_append_(self, slot);
+        FUGA_CHECK(FugaParser_slot_(parser, self));
         needsep = true;
     }
     return self;
@@ -487,6 +399,19 @@ TESTS(FugaParser) {
         &&  Fuga_hasLength_(Fuga_getI(self, 0), 2)
         &&  Fuga_isInt (Fuga_getI(self, 1))
         );
+    FUGA_PARSER_TEST("(:: foo\n 10)",
+           !Fuga_isRaised(self)
+        &&  Fuga_hasLength_(self, 1)
+        &&  Fuga_hasDocI(self, 0)
+        &&  FugaString_is_(Fuga_getDocI(self, 0), "foo\n")
+        &&  FugaInt_is_(Fuga_getI(self, 0), 10));
+    FUGA_PARSER_TEST("(:: foo  \n    ::bar\n 20)",
+           !Fuga_isRaised(self)
+        &&  Fuga_hasLength_(self, 1)
+        &&  Fuga_hasDocI(self, 0)
+        &&  FugaString_is_(Fuga_getDocI(self, 0), "foo  \nbar\n")
+        &&  FugaInt_is_(Fuga_getI(self, 0), 20));
+
 
     Fuga_quit(self);
 }
--- src/fuga/parser.h
+++ src/fuga/parser.h
@@ -40,36 +40,6 @@ bool FugaParser_readFile_(
 );
 
 /**
-*** ### FugaParser_operators
-***
-*** Return the Parser's operators metadata object.
-**/
-void* FugaParser_operators(
-    FugaParser* self
-);
-
-/**
-*** ### FugaParser_infixr_precedence_
-*** 
-*** Set a right-associative operator's precedence.
-**/
-void FugaParser_infix_precedence_(
-    FugaParser* self,
-    const char* op,
-    size_t precedence
-);
-
-/**
-*** ### FugaParser_precedence_
-***
-*** Return an operator's precedence.
-**/
-size_t FugaParser_precedence_(
-    FugaParser* self,
-    const char* op
-);
-
-/**
 *** ### FugaParser_block
 ***
 *** Parse a block (i.e., an object without the parentheses).
@@ -86,17 +56,6 @@ void* FugaParser_block(
 void* FugaParser_expression(
     FugaParser* self
 );
-
-/**
-*** ### FugaParser_expression_
-***
-*** Parse a single Fuga expression at a given binding power.
-**/
-void* FugaParser_expression_(
-    FugaParser* self,
-    size_t rbp
-);
-
 
 #endif
 
